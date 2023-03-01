1cae77774dcc30d1722c2f1bdf959731de10ef30
trang705105127 <phamtrang2042@gmail.com>
2023-03-01 08:01:24 +0700

    Phien ban dau tien

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include "code.h"
+void sap_xep_day_so(int* a, int n)
+{
+// ma lenh sap xep day so
+}
+int tim_max(int* a, int n)
+{
+// ma lenh ham tim max
+}
+int main()
+{
+cout << "Phien ban 2";
+return 0;
+}
